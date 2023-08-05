2ab8bc5d7369eb2263c65c15643284a9c3b66602
Emir Tactuk Paulino <33699141+TatuninoDev12@users.noreply.github.com>
2023-08-04 22:36:25 -0400

    init push

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+const CustomButton = ({ title, type, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        background: "linear-gradient(90deg, #FFD803 0%, #FFA500 100%)",
+        color: "#000",
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
+      };
+    }
+  };
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
